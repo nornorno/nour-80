a416fb791d6af74966ebd4748966df414a167804
nornorno <noureteo@gmail.com>
2024-06-06 15:13:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/noooortog/Cr-noor",
+    "https://github.com/nornorno/nour-80",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
